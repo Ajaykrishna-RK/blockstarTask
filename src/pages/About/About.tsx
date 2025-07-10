2a4a05bf94a38c9109e533c675c81eb4fd4db224
Ajay <ajayajuu73@gmail.com>
2025-07-10 10:16:21 +0530

    complete

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -5,9 +5,9 @@ function About() {
     <div className=" flex items-center  flex-col justify-center p-4">
       <p className="text-3xl font-bold ">About Page</p>
       <p className="text-lg mb-2 mt-2 text-gray-800 max-w-2xl text-center">
-        This React application is part of a task that demonstrates basic
-        concepts like routing, navigation, and conditional rendering. It
-        includes a simple interactive game and pages styled with Tailwind CSS.
+        This is a simple React app showcasing routing and component structure.
+        Use the links below to explore the Home section and play the Head & Tail
+        game.
       </p>
 
       <HeaderLinks excludeIndex={1} />
